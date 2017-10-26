61c0d6bbec01391f3041c1c24c82bcca3f0db9c2
Richard Burd <richardburdx@yahoo.com>
2017-10-26 02:39:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Fido"
+  end 
 end
+
+
+
+
+
+
+#class App < Sinatra::Base
+
+  # Define your GET '/' route below and respond with "Hello, World!"
+#  get '/' do
+#    "Hello, World!"
+#  end
+#end
