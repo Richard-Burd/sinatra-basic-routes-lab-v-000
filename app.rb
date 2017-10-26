63a1f4ee375e1cdad031e67803b1b830a606aee4
Richard Burd <richardburdx@yahoo.com>
2017-10-26 02:42:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Fido"
   end
+
+  get '/hometown' do 
+    "My hometown is Valletta"
+  end 
+
+  get '/favorite-song' do 
+    "My favorite song is Deus Ta Malta"
+  end 
 end
 
 
